3e5d07ccdb4d824df56815aea936ba182fb9bf86
yixing233-giga <2191453326@qq.com>
2026-09-16 18:06:00 +0800

    chore: release v1.1.2

--- src/utils/versionCheck.ts
+++ src/utils/versionCheck.ts
@@ -2,7 +2,7 @@
  * 版本检测工具：从项目静态元数据源读取线上版本
  */
 
-export const CURRENT_VERSION = '1.1.1';
+export const CURRENT_VERSION = '1.1.2';
 export const REPO_OWNER = 'yixing233';
 export const REPO_NAME = 'CrabTab';
 export const GITHUB_REPO_URL = `https://github.com/${REPO_OWNER}/${REPO_NAME}`;
